babd93891c8553f262f8ef05c7e96ba26c74e034
Valerie Foster <fosterv2@outlook.com>
2020-04-02 00:55:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,11 +9,11 @@ end
 
 def assign_rooms(speakers)
   assignments = []
-  speakers.each_with_index { |name, num| assignments << "Hello, #{name}! You'll be assigned to room #{num}!" }
+  speakers.each_with_index { |name, num| assignments << "Hello, #{name}! You'll be assigned to room #{num + 1}!" }
   assignments
 end
 
-def printer(badge_array, rooms_array)
-  badge_array.each { |badge| puts badge }
-  rooms_array.each { |room| puts room }
+def printer(speakers)
+  batch_badge_creator(speakers).each { |badge| puts badge }
+  assign_rooms(speakers).each { |room| puts room }
 end
