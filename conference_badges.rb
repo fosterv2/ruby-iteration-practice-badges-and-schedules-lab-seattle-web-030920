6ca59ebb31f6c07a4e99684722f99990c61bdb11
Valerie Foster <fosterv2@outlook.com>
2020-04-02 00:49:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+  speakers.map { |name| badge_maker(name) }
+end
+
+def assign_rooms(speakers)
+  assignments = []
+  speakers.each_with_index { |name, num| assignments << "Hello, #{name}! You'll be assigned to room #{num}!" }
+  assignments
+end
